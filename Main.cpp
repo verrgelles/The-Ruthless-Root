a0701533dc3f03d399bec0a66697271514586296
Simon Vergeles <verrgelles@gmail.com>
2023-04-21 21:49:33 +0300

    Разработана функция

--- Main.cpp
+++ Main.cpp
@@ -1,52 +1,27 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int max_energy = 134; // in MEv
+int max_energy = 134; // MEV
+int speed_of_light = 1;
 
 auto Generator(int max_energy, int speed_of_light, int particle_number)
 {
-    bool f = 0;
-    auto rand = new TRandom3();
+    auto r = new TRandom(0);
 
     double E_1, E_2, P_1, P_2, phi_1, phi_2, rho_1, rho_2;
 
-    while (f)
-    {
-        E_1 = rand->Rndm() * 100;
-        if (E_1 > 0 && E_1 < max_energy)
-        {
-            E_2 = max_energy - E_1;
+    E_1 = r->Uniform(1, max_energy);
+    E_2 = max_energy - E_1;
 
-            P_1 = E_1 / 2 * speed_of_light;
-            P_2 = E_2 / 2 * speed_of_light;
+    P_1 = E_1 / 2 * speed_of_light;
+    P_2 = E_2 / 2 * speed_of_light;
 
-            f = 1;
-        }
-    };
+    phi_1 = r->Uniform(1, 2 * TMath::Pi());
+    phi_2 = phi_1 + TMath::Pi();
 
-    f = 0;
-    while (f)
-    {
-        phi_1 = rand->Rndm() * 1.5;
-        if (phi_1 > 0 && phi_1 < 2 * TMath::Pi())
-        {
-            phi_2 = phi_1 + TMath::Pi();
-            f = 1;
-        }
-    };
-
-    f = 0;
-    while (f)
-    {
-        rho_1 = rand->Rndm() * 1.5;
-        if (rho_1 > 0 && rho_1 < 2 * TMath::Pi())
-        {
-            rho_2 = rho_2 + TMath::Pi();
-            f = 1;
-        }
-    }
+    rho_1 = r->Uniform(1, 2 * TMath::Pi());
+    rho_2 = rho_1 + TMath::Pi();
 
     double ptc1_px = P_1 * TMath::Cos(phi_1);
     double ptc1_py = P_1 * TMath::Sin(phi_1);
@@ -67,6 +42,22 @@ auto Generator(int max_energy, int speed_of_light, int particle_number)
 
 int Main()
 {
+    TCanvas *c1 = new TCanvas();
+
+    TH1D *hist = new TH1D("hist", "Histro", 5000, 1, 134);
+
+    for (int i = 0; i < 5000; i++)
+    {
+        auto ptc_1 = Generator(max_energy, speed_of_light, 1);
+        auto ptc_2 = Generator(max_energy, speed_of_light, 2);
+
+        hist->Fill(ptc_1.E());
+    }
+
+    hist->GetXaxis()->SetTitle("E");
+    hist->GetYaxis()->SetTitle("Number of particles");
+
+    hist->Draw();
 
     return 0;
 }
