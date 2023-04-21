e0b24fc683528ca6456981feef5fd57f18a936ac
Simon Vergeles <verrgelles@gmail.com>
2023-04-21 19:10:50 +0300

    C->CPP

--- Main.C
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int run()
-{
-    return 0;
-}
---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int Generator()
+{
+    return 0;
+}
+
+int Main()
+{
+
+    return 0;
+}
